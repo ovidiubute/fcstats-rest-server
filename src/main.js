81779b1da5afff0adacefc5e9545cce49e470006
ovidiu <ovidiu.bute@gmail.com>
2016-05-08 15:46:04 +0300

    Trying to fix mem leak.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ const Match = require('fcstats-persistence').Match;
 const persistence = require('fcstats-persistence').lib;
 
 let db;
+persistence.loadDatabase('./node_modules/fcstats-dbimporter/out/fcstats0_4.json').then((database) => {
+  db = database;
+});
+
 const server = restify.createServer({
   name: 'fcstats-rest-server',
   version: '2.0.0'
@@ -12,15 +16,8 @@ const server = restify.createServer({
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
-server.use((req, res, next) => {
-  persistence.loadDatabase('./node_modules/fcstats-dbimporter/out/fcstats0_4.json').then((database) => {
-    db = database;
-    next();
-  })
-});
 
 server.get('/api/matches/:leagueName/:seasonYear', function (req, res, next) {
-  console.log(db);
   const year = Number.parseInt(req.params.seasonYear);
   const league = req.params.leagueName;
 
